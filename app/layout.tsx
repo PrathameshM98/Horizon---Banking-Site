16d9a8edec777589e760c1862ac0520fe0cf424b
Prathamesh Matte <prathameshmatte1683@gmail.com>
2024-07-12 14:51:51 -0400

    add force dynamics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,22 @@
+export const dynamic = "force-dynamic";
+
 import type { Metadata } from "next";
 import { Inter, IBM_Plex_Serif } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 const ibmPlexSerif = IBM_Plex_Serif({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  variable: '--font-ibm-plex-serif'
-})
-
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-ibm-plex-serif",
+});
 
 export const metadata: Metadata = {
   title: "Horizon Bank",
   description: "Horizon is a modern banking platform for everyone.",
   icons: {
-    icon: '/icon/logo.svg'
-  }
+    icon: "/icon/logo.svg",
+  },
 };
 
 export default function RootLayout({
@@ -25,7 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>{children}</body>
+      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>
+        {children}
+      </body>
     </html>
   );
 }
